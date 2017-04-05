7b67209b85fe538c54e7171dd23177b219e742fd
Filip Gdovin <f.gdovin@gmail.com>
2017-04-05 22:29:26 +0200

    added Spring Boot

---
+++ logging-boot-app/src/main/java/cz/muni/pa036/logging/PA036Application.java
@@ -0,0 +1,15 @@
+package cz.muni.pa036.logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+@SpringBootApplication
+@EnableAutoConfiguration
+public class PA036Application {
+    public static void main(String[] args) {
+        SpringApplication.run(PA036Application.class, args);
+    }
+}
