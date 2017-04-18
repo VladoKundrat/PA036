16b3188ab169c99eb885fc66fc553b64e55d2d51
kamil-triscik <kamil28193@gmail.com>
2017-04-18 21:58:17 +0200

    spring-boot

--- logging-boot-app/src/main/java/cz/muni/pa036/logging/PA036Application.java
+++ logging-boot-app/src/main/java/cz/muni/pa036/logging/PA036Application.java
@@ -3,12 +3,24 @@ package cz.muni.pa036.logging;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@ComponentScan
-@SpringBootApplication
+@Configuration
+@ComponentScan(basePackages="cz.muni.pa036.logging")
+@SpringBootApplication(scanBasePackages={"cz.muni.pa036.logging"})
 @EnableAutoConfiguration
-public class PA036Application {
+//@EntityScan(basePackages="cz.muni.pa036.logging")
+public class PA036Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PA036Application.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(PA036Application.class, args);
     }
